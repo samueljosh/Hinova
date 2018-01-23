f4abb733003c9ea7cb849daf843e8b98bc9ef2ac
samueljosh <samueljoshua@Live.com>
2018-01-23 14:03:21 -0200

    Update BundleConfig.cs

--- AplicacaoHinova/AplicacaoHinova/App_Start/BundleConfig.cs
+++ AplicacaoHinova/AplicacaoHinova/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace AplicacaoHinova
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
